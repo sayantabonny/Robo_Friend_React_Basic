1874e4cd59977de15ca1e4e5e88e3cf725e76ef6
sayantabonny <sayantabonny@gmail.com>
2021-05-19 17:19:21 +0530

    Completed

--- robofriend/src/App.js
+++ robofriend/src/App.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {robots} from './robots';
 import SearchBox from './SearchBox'
 import CardList from './CardList';
 
-const App = () => {
 
-  return (
-    <div className='tc'>
-      <h1>Robot Friends</h1>
-      <SearchBox />
-      <CardList robots={robots}/>
-    </div>
-  )
+class App extends Component {
+  
+  constructor(){
+    super()
+    this.state={
+      robots: robots,
+      searchField: ''
+    }
+    
+  }
+
+  onSearchChange = (event) =>{
+
+    this.setState({searchField: event.target.value})
+    console.log(event.target.value);
+
+    
+  }
+
+  render (){
+    const filteredRobot= this.state.robots.filter(robots=>
+      {
+        return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+      })
+
+    return (
+      <div className='tc'>
+        <h1>Robot Friends</h1>
+        <SearchBox SearchChange={this.onSearchChange}/>
+        <CardList robots={filteredRobot}/>
+      </div>
+    )
+  }
 }
+
+  
+
 
 export default App;
--- robofriend/src/SearchBox.js
+++ robofriend/src/SearchBox.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 
-const SearchBox=()=>{
+const SearchBox=({searchfield, SearchChange})=>{
 
     return(
         <div className='pa2'>
-            <input className='pa3 ba b--green bg--lightest-blue' type='search' placeholder='Search Robots'/>
+            <input className='pa3 ba b--green bg--lightest-blue' type='search' placeholder='Search Robots' onChange={SearchChange}/>
         </div>
     )
 }
